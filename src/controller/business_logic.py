fd6c9dc3816e19e5046257ed94e1f148218d68b2
Derik Daniel Daza <derik3d@gmail.com>
2024-09-11 04:13:23 -0500

    update logic file

--- src/controller/business_logic.py
+++ src/controller/business_logic.py
@@ -1,26 +1,38 @@
 """buisness logic -> before data manipulation"""
-import dao.data_product as data
 
-from dao.data_order import get_all_documents
+import dao.data_product as data_products
+import dao.data_order as data_orders
 
 #PRODUCTS
 
 def list_products():
-    return data.list_products()
+    return data_products.list_products()
 
 def get_product(id):
-    return data.get_product(id)
+    return data_products.get_product(id)
 
-def create_product(data):
-    return data.create_product(data)
+def create_product(body):
+    return data_products.create_product(body)
 
-def update_product(id, data):
-    return data.update_product(id, data)
+def update_product(id, body):
+    return data_products.update_product(id, body)
 
 def delete_product(id):
-    return data.delete_product(id)
+    return data_products.delete_product(id)
 
 #ORDERS
 
 def list_orders():
-    return get_all_documents()
+    return data_orders.list_orders()
+
+def get_order(id):
+    return data_orders.get_order(id)
+
+def create_order(body):
+    return data_orders.create_order(body)
+
+def update_order(id, body):
+    return data_orders.update_order(id, body)
+
+def delete_order(id):
+    return data_orders.delete_order(id)
