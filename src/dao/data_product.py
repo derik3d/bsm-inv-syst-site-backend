3bb856940a74e43aa813c553b5ae62edb88c63df
Derik Daniel Daza <derik3d@gmail.com>
2024-09-12 17:30:27 -0500

    refactor sql crud

--- src/dao/data_product.py
+++ src/dao/data_product.py
@@ -19,93 +19,74 @@ def get_db_connection():
     return connection
 
 def list_general(table):
-    # Connect to the MySQL database
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(f"SELECT * FROM {table}")
-    rows = cursor.fetchall()
-
-    column_names = [i[0] for i in cursor.description]
-    print("Column Names:", column_names)
-
-    res = []
-    
-    for row in rows:
-        # Create a dictionary for each row
-        row_dict = dict(zip(column_names, row))
-        res.append(row_dict)
-    
-    # Close the connection
-    cursor.close()
-    conn.close()
-    
-    # Convert to JSON and return
-    return res
+    query = "SELECT * FROM " + table
+    products=execute_query(query, fetch=2)
+    return products
 
 def get_general(table,id):
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor(dictionary=True)
-        cursor.execute('SELECT * FROM '+ table +' WHERE id = %s', (id,))
-        product = cursor.fetchone()
-        cursor.close()
-        conn.close()
-        if product:
-            return jsonify(product), 200
-        else:
-            return jsonify({'message': 'Product not found'}), 404
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+    query = 'SELECT * FROM '+ table + ' WHERE id = %s'
+    values = (id,)
+    product = execute_query(query,values, fetch=1)
+    return product
 
 def create_product(data):    
+
     product_name = data.model_dump()['product_name']
     product_description = data.model_dump()['product_description']
     fk_product_type_id = data.model_dump()['fk_product_type_id']
-    #fk_product_type_id
     
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
+    query = 'INSERT INTO product (product_name, product_description, fk_product_type_id) VALUES (%s, %s, %s)'
+    values =  (product_name, product_description,fk_product_type_id)
+    execute_query(query,values)
+    return jsonify({'message': 'Product created successfully!'}), 201
 
-        query = 'INSERT INTO product (product_name, product_description, fk_product_type_id) VALUES (%s, %s, %s)'
 
-        cursor.execute(query, (product_name, product_description,fk_product_type_id))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return jsonify({'message': 'Product created successfully!'}), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+def update_product(id,data):  
 
-def update_product(id,data):
-    name = data['name']
-    description = data['description']
-    
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('UPDATE product SET name = %s, description = %s WHERE id = %s', (name, description, id))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return jsonify({'message': 'Product updated successfully!'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    product_name = data.model_dump()['product_name']
+    product_description = data.model_dump()['product_description']
+    fk_product_type_id = data.model_dump()['fk_product_type_id']
+
+    query='UPDATE product SET product_name = %s, product_description = %s,  fk_product_type_id = %s WHERE id = %s'
+    values=(product_name, product_description,fk_product_type_id,id)
+    execute_query(query,values)
+    return jsonify({'message': 'Product updated successfully!'}), 200
+
 
 def delete_product(id):
+    query = 'DELETE FROM product WHERE id = %s'
+    values = id
+    execute_query(query,values)
+    return jsonify({'message': 'Product deleted'}), 201
+
+def execute_query(query,values=None,fetch=0):
     try:
+        result=None
         conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('DELETE FROM product WHERE id = %s', (id,))
-        conn.commit()
+        if fetch==0:
+            cursor = conn.cursor()
+            cursor.execute(query,values)
+            conn.commit()
+        if fetch==1:
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute(query,values)
+            result = cursor.fetchone()
+        if fetch==2:
+            cursor = conn.cursor()
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            column_names = [i[0] for i in cursor.description]
+            print("Column Names:", column_names)
+            result = []
+            for row in rows:
+                # Create a dictionary for each row
+                row_dict = dict(zip(column_names, row))
+                result.append(row_dict)
+
         cursor.close()
         conn.close()
-        if cursor.rowcount > 0:
-            return jsonify({'message': 'Product deleted successfully!'}), 200
-        else:
-            return jsonify({'message': 'Product not found'}), 404
+        return result
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-    
+
+
